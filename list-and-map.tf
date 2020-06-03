4edb7ca54a1c83c10912a4d790ed479d16378f0d
Ananya Palit <anonyo.2014@gmail.com>
2020-06-03 00:34:52 -0700

    Formatted the file

--- list-and-map.tf
+++ list-and-map.tf
@@ -1,25 +1,25 @@
 resource "aws_instance" "build-server" {
-  ami = "ami-0e34e7b9ca0ace12d"
+  ami           = "ami-0e34e7b9ca0ace12d"
   instance_type = var.build-server-instance-types[2]
-  tags = {"Name": "build-server"}
+  tags          = { "Name" : "build-server" }
 }
 
 variable "build-server-instance-types" {
-  type = list
+  type    = list
   default = ["t2.micro", "t2.nano", "t2.medium"]
 }
 
 resource "aws_instance" "spark-node" {
-  ami = "ami-0e34e7b9ca0ace12d"
+  ami           = "ami-0e34e7b9ca0ace12d"
   instance_type = var.spark-node-instance-types["beta"]
-  tags = {"Name": "spark-node"}
+  tags          = { "Name" : "spark-node" }
 }
 
 variable "spark-node-instance-types" {
   type = map
   default = {
     "poc" : "r5.large"
-    "beta": "r5.xlarge"
-    "prod": "r5.4xlarge"
+    "beta" : "r5.xlarge"
+    "prod" : "r5.4xlarge"
   }
 }
